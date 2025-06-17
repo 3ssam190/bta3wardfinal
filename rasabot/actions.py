9487f57c79ba8659286eda88cf3a79bc524a2bda
3ssam190 <105178785+3ssam190@users.noreply.github.com>
2025-06-17 07:25:49 +0300

    Delete rasabot directory

--- rasabot/actions.py
+++
@@ -1,8 +0,0 @@
-from rasa_sdk import Action
-
-class ActionSearchProducts(Action):
-    def name(self):
-        return "action_search_products"
-
-    def run(self, dispatcher, tracker, domain):
-        return []  # All logic is now handled in PHP
